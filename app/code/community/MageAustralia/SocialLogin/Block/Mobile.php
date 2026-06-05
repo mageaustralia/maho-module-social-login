e45b7896dd4280f518c425af078bfbdc7e0d9095
Matthew Campbell <mcampbell@mageaustralia.com.au>
2026-06-05 14:17:14 +1000

    feat(otp): account-page add/verify mobile UI for SMS sign-in

---
+++ app/code/community/MageAustralia/SocialLogin/Block/Mobile.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+class MageAustralia_SocialLogin_Block_Mobile extends Mage_Core_Block_Template
+{
+    public function isEnabled(): bool
+    {
+        $helper = Mage::helper('sociallogin');
+        return Mage::getSingleton('customer/session')->isLoggedIn()
+            && $helper->isOtpEnabled() && $helper->isOtpSmsEnabled();
+    }
+
+    public function getCurrentMobile(): string
+    {
+        return (string) Mage::getSingleton('customer/session')->getCustomer()->getMobile();
+    }
+
+    public function isMobileVerified(): bool
+    {
+        return (bool) Mage::getSingleton('customer/session')->getCustomer()->getMobileVerified();
+    }
+
+    public function getRequestUrl(): string
+    {
+        return $this->getUrl('sociallogin/otp/request');
+    }
+
+    public function getVerifyUrl(): string
+    {
+        return $this->getUrl('sociallogin/otp/add-mobile');
+    }
+
+    public function getFormKey(): string
+    {
+        return Mage::getSingleton('core/session')->getFormKey();
+    }
+}
